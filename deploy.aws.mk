def7241cd07485f2ec5d37a587df0d64129c05e7
Tharun <tharunvar10@gmail.com>
2022-08-05 20:37:44 +0530

    aws deployment commands

--- deploy.aws.mk
+++ deploy.aws.mk
@@ -2,16 +2,19 @@
 CLUSTER_NAME=$(K8NS)
 K8S_VERSION=1.22
 REGION=ap-south-1
-BASE_REPO_NAME=$(PROJECT_NAME)-$(ENVIRONMENT)
+BASE_REPO_NAME=$(PROJECT_NAME)-$(ENVIROMENT)
 
 AWS_ACCOUNT_ID?=$(shell aws sts get-caller-identity --query "Account" --output text)
 IMAGE_REGISTRY?=$(AWS_ACCOUNT_ID).dkr.ecr.$(REGION).amazonaws.com
 
-ORDERS_SERVICE_IMAGE_REPO_NAME=$(BASE_REPO_NAME)/$(ORDERS_SERVICE_NAME)
-ORDERS_SERVICE_IMAGE_REPO_URL=$(IMAGE_REGISTRY)/$(ORDERS_SERVICE_IMAGE_REPO_NAME):$(ORDERS_SERVICE_VERSION)
+ORDERS_SERVICE_REPO_NAME=$(BASE_REPO_NAME)/$(ORDERS_SERVICE_NAME)
+ORDERS_SERVICE_IMAGE_REPO_URL=$(IMAGE_REGISTRY)/$(ORDERS_SERVICE_REPO_NAME):$(ORDERS_SERVICE_VERSION)
 
-CUSTOMERS_SERVICE_IMAGE_REPO_NAME=$(BASE_REPO_NAME)/$(CUSTOMERS_SERVICE_NAME)
-CUSTOMERS_SERVICE_IMAGE_REPO_URL=$(IMAGE_REGISTRY)/$(CUSTOMERS_SERVICE_IMAGE_REPO_NAME):$(CUSTOMERS_SERVICE_VERSION)
+CUSTOMERS_SERVICE_REPO_NAME=$(BASE_REPO_NAME)/$(CUSTOMERS_SERVICE_NAME)
+CUSTOMERS_SERVICE_IMAGE_REPO_URL=$(IMAGE_REGISTRY)/$(CUSTOMERS_SERVICE_REPO_NAME):$(CUSTOMERS_SERVICE_VERSION)
+
+local-machine-kubectl-config:
+	aws eks update-kubeconfig --region $(REGION) --name $(CLUSTER_NAME)
 
 push-orders-service-registry-image:
 	docker tag $(ORDERS_SERVICE_IMAGE_NAME) $(ORDERS_SERVICE_IMAGE_REPO_URL)
@@ -20,6 +23,8 @@ push-orders-service-registry-image:
 push-customers-service-registry-image:
 	docker tag $(CUSTOMERS_SERVICE_IMAGE_NAME) $(CUSTOMERS_SERVICE_IMAGE_REPO_URL)
 	docker push $(CUSTOMERS_SERVICE_IMAGE_REPO_URL)
+
+push-service-images:	push-orders-service-registry-image push-customers-service-registry-image
 
 delete-cluster:
 	eksctl delete cluster --name=$(CLUSTER_NAME) --region=$(REGION)
@@ -35,13 +40,18 @@ create-cluster:
 		  --nodes 1
 
 
+
+login-registry:
+	aws ecr get-login-password --region $(REGION) | docker login --username AWS --password-stdin $(IMAGE_REGISTRY)
+
+
 create-registry-repositories:
 	- aws ecr create-repository --repository-name $(CUSTOMERS_SERVICE_IMAGE_REPO_NAME) --region $(REGION)
-	- aws ecr create-repository --repository-name $(ORDERS_SERVICE_IMAGE_REPO_NAME) --region $(REGION)
+	- aws ecr create-repository --repository-name $(ORDERS_SERVICE_REPO_NAME) --region $(REGION)
 
-delete-registryrepositories:
+delete-registry-repositories:
 	- aws ecr delete-repository --force --repository-name $(CUSTOMERS_SERVICE_IMAGE_REPO_NAME) --region $(REGION)
-	- aws ecr delete-repository --force --repository-name $(ORDERS_SERVICE_IMAGE_REPO_NAME) --region $(REGION)
+	- aws ecr delete-repository --force --repository-name $(ORDERS_SERVICE_REPO_NAME) --region $(REGION)
 
 env-up:	create-cluster
 
