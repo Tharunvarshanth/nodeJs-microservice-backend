f41effe3dd55be120d5e19d273551c7dc983cabe
Tharun <tharunvar10@gmail.com>
2022-08-09 12:42:54 +0530

    fixed init sql issue

--- microservice/deployment/db/init.sql
+++ microservice/deployment/db/init.sql
@@ -1,4 +1,3 @@
-CREATE USER 'keycloakuser' IDENTIFIED BY 'keycloak';
+CREATE USER 'keycloakuser'@'%' IDENTIFIED BY 'keycloak';
 create database keycloak;
-GRANT ALL PRIVILEGES ON keycloak TO 'keycloakuser'@'%' WITH GRANT OPTION;
-/*GRANT ALL PRIVILEGES ON  keycloak TO 'keycloakuser';*/
+GRANT ALL PRIVILEGES ON *.* TO 'keycloakuser'@'%';
