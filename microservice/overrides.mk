c2131857c11aa2639652e6193c97c1f4e2454e6b
Tharun <tharunvar10@gmail.com>
2022-08-09 18:40:05 +0530

    keycload issue fixes

--- microservice/overrides.mk
+++ microservice/overrides.mk
@@ -2,11 +2,11 @@
 ENVIROMENT=dev
 PROJECT_NAME=booking
 
-APP_HOST=
+APP_HOST=localhost
 
 DB_HOST=booking-db-mysql
 DB_PORT="3306"
 DB_USER=root
 DB_PASSWORD=root
 
-FRONTEND_URL=localhost
+FRONTEND_URL=http://localhost/auth/
